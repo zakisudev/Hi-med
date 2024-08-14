8cb4bf33134772bee07457eb0c56c15fbb26e5e9
zakisudev <zakisudev@gmail.com>
2024-08-14 22:41:10 +0300

    feat: Update imports to use absolute paths
    This commit updates the imports in the `about/page.tsx` and `book/page.tsx` files to use absolute paths instead of relative paths. The `Header` and `Footer` components are now imported from the `@/components` directory, improving the codebase's organization and making it easier to refactor in the future.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
-import Header from '../components/Header';
+import Header from '@/components/Header';
 import Image from 'next/image';
-import Footer from '../components/Footer';
+import Footer from '@/components/Footer';
 
 export default function About() {
   return (
--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Link from 'next/link';
-import Image from 'next/image';
-import Footer from '../components/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function Book() {
   return (

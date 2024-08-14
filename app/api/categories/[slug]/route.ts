ab4d2e868c197d1472e87a44136746b3b8a88650
zakisudev <zakisudev@gmail.com>
2024-08-14 22:54:14 +0300

    refactor: Update category API routes and handlers

--- app/api/categories/[slug]/route.ts
+++ app/api/categories/[slug]/route.ts
@@ -2,46 +2,47 @@ import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 import { extractSlugFromUrl } from '@/app/utils/helpers';
 
-export async function handler(req: Request) {
-  if (req.method === 'GET') {
-    try {
-      const slug = extractSlugFromUrl(req.url || '');
+// Rename the function to match the HTTP method
+export async function GET(req: Request) {
+  try {
+    const slug = extractSlugFromUrl(req.url || '');
 
-      if (typeof slug !== 'string' || !slug.trim()) {
-        return NextResponse.json(
-          { message: 'Invalid category slug' },
-          { status: 400 }
-        );
-      }
-
-      const category = await prisma.category.findUnique({
-        where: {
-          name: slug.toLowerCase()
-        }
-      });
-
-      if (!category) {
-        return NextResponse.json(
-          { message: 'No category found' },
-          { status: 404 }
-        );
-      }
-
-      return NextResponse.json({
-        ...category,
-        content: JSON.parse(category.content as string),
-      });
-    } catch (error) {
-      console.log(error);
+    if (typeof slug !== 'string' || !slug.trim()) {
       return NextResponse.json(
-        { message: 'Something went wrong' },
-        { status: 500 }
+        { message: 'Invalid category slug' },
+        { status: 400 }
       );
     }
-  } else {
+
+    const category = await prisma.category.findUnique({
+      where: {
+        name: slug.toLowerCase(),
+      },
+    });
+
+    if (!category) {
+      return NextResponse.json(
+        { message: 'No category found' },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      ...category,
+      content: JSON.parse(category.content as string),
+    });
+  } catch (error) {
+    console.log(error);
     return NextResponse.json(
-      { message: 'Method not allowed' },
-      { status: 200 }
+      { message: 'Something went wrong' },
+      { status: 500 }
     );
   }
+}
+
+export async function POST(req: Request) {
+  return NextResponse.json(
+    { message: 'Method not allowed' },
+    { status: 405 }
+  );
 }
--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,7 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function handler(req: NextRequest) {
+export async function GET(req: NextRequest) {
   if (req.method === 'GET') {
     try {
       const categories = await prisma.category.findMany();
@@ -25,50 +25,52 @@ export async function handler(req: NextRequest) {
         { status: 200 }
       );
     }
-  } else if (req.method === 'POST') {
-    try {
-      const { name, image, description, content } = await req.json();
-
-      if (!name) {
-        return NextResponse.json(
-          { message: 'Name is required' },
-          { status: 200 }
-        );
-      }
-      if (!description) {
-        return NextResponse.json(
-          { message: 'Description is required' },
-          { status: 200 }
-        );
-      }
-      if (!content || !Array.isArray(content)) {
-        return NextResponse.json(
-          { message: 'Content is required' },
-          { status: 200 }
-        );
-      }
-
-      const category = await prisma.category.create({
-        data: { name, description, image, content: JSON.stringify(content) },
-      });
-
-      if (!category) {
-        return NextResponse.json(
-          { message: 'Category not created' },
-          { status: 200 }
-        );
-      }
-      return NextResponse.json({ category }, { status: 200 });
-    } catch (error) {
-      console.log(error);
-      return NextResponse.json(
-        { message: 'Something went wrong' },
-        { status: 200 }
-      );
-    }
   } else {
     return NextResponse.json(
       { message: 'Method not allowed' },
+      { status: 200 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { name, image, description, content } = await req.json();
+
+    if (!name) {
+      return NextResponse.json(
+        { message: 'Name is required' },
+        { status: 200 }
+      );
+    }
+    if (!description) {
+      return NextResponse.json(
+        { message: 'Description is required' },
+        { status: 200 }
+      );
+    }
+    if (!content || !Array.isArray(content)) {
+      return NextResponse.json(
+        { message: 'Content is required' },
+        { status: 200 }
+      );
+    }
+
+    const category = await prisma.category.create({
+      data: { name, description, image, content: JSON.stringify(content) },
+    });
+
+    if (!category) {
+      return NextResponse.json(
+        { message: 'Category not created' },
+        { status: 200 }
+      );
+    }
+    return NextResponse.json({ category }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Something went wrong' },
       { status: 200 }
     );
   }

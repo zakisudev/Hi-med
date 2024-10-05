0e53f366225ea9105f39eee29291271494580210
zakisudev <zakisudev@gmail.com>
2024-10-05 09:50:21 +0300

    refactor: Remove unused Header component from Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,8 +14,6 @@ export default function Contact() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-
       <main className="flex-1 flex min-h-screen flex-col w-full justify-start items-center">
         <section className="flex w-full h-[450px] relative mb-20">
           <div

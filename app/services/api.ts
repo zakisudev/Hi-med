9031e5f2ffe6ae56884f628cad510eae4ff942e6
zakisudev <zakisudev@gmail.com>
2024-10-08 14:48:10 +0300

    Refactor API service: Update BASE_URL to handle different environments

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,10 +1,13 @@
-const BASE_URL = "https://hi-med.zakisu.com/api";
+const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://hi-med.zakisu.com/api';
 
 export const fetchCategories = async () => {
   const res = await fetch(`${BASE_URL}/categories`);
 
   if (!res.ok) {
-    return "An error occurred while fetching categories";
+    return 'An error occurred while fetching categories';
   }
 
   return await res.json();
@@ -14,7 +17,7 @@ export const fetchCategoryData = async (name: string) => {
   const res = await fetch(`${BASE_URL}/categories/${name}`);
 
   if (!res.ok) {
-    return "An error occurred while fetching category data";
+    return 'An error occurred while fetching category data';
   }
 
   return await res.json();
